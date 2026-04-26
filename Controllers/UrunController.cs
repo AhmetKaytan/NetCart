c531b21f75a4404af9097f234ec05076f3c6f3b2
Ahmet <ahmetkaytan3599@gmail.com>
2026-04-26 15:52:56 +0300

    Styling and basic design for home index

---
+++ Controllers/UrunController.cs
@@ -0,0 +1,19 @@
+using dotnet_store.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_store.Controllers;
+
+public class UrunController : Controller
+{
+    private readonly DataContext _context;
+
+    public UrunController(DataContext context)
+    {
+        _context = context;
+    }
+    public IActionResult Index()
+    {
+        List<Urun> urunler = _context.Urunler.ToList();
+        return View(urunler);
+    }
+}
